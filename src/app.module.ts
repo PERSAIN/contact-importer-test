2978ab9f618c17505c837dff44a34e3ad141f9a4
Camilo Pérez <camiloperez9702@gmail.com>
2022-02-12 00:45:25 -0500

    adding postgres and typeorm as database for our app, configModule from nest to have a configuration module in our app that is able to handle different variables depending from the environment and to give an extra security we are using joi to create a validationSchema of what we need in our server to run

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,33 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ContactImporterModule } from './contact-importer/contact-importer.module';
+import { configValidationSchema } from './config.schema';
 
 @Module({
-  imports: [ContactImporterModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: [`.env.stage.${process.env.STAGE}`],
+      validationSchema: configValidationSchema,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        return {
+          type: 'postgres',
+          autoLoadEntities: true,
+          synchronize: true,
+          host: configService.get('DB_HOST'),
+          port: configService.get('DB_PORT'),
+          username: configService.get('DB_USERNAME'),
+          password: configService.get('DB_PASSWORD'),
+          database: configService.get('DB_DATABASE'),
+        };
+      },
+    }),
+    ContactImporterModule,
+  ],
   controllers: [],
   providers: [],
 })
